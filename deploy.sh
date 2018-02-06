ccb01c6d9d40d872eee45996a34da6a124945f9f
David Bush <dbush@redhat.com>
2018-02-06 10:27:14 -0500

    created a ping controller to check if site is up and incremented version

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-VERSION="0.6"
-#scp -C schedulerapi/target/scheduler-api-${VERSION}.jar ubuntu@ec2-54-91-172-100.compute-1.amazonaws.com:/usr/local/scheduler
-#scp -C schedulerservice/target/scheduler-service-${VERSION}.jar ubuntu@ec2-54-91-172-100.compute-1.amazonaws.com:/usr/local/scheduler
+VERSION="0.8"
+scp -C schedulerapi/target/scheduler-api-${VERSION}.jar ubuntu@ec2-54-91-172-100.compute-1.amazonaws.com:/usr/local/scheduler
+scp -C schedulerservice/target/scheduler-service-${VERSION}.jar ubuntu@ec2-54-91-172-100.compute-1.amazonaws.com:/usr/local/scheduler
 ssh ubuntu@ec2-54-91-172-100.compute-1.amazonaws.com 'rm -f /usr/local/scheduler/scheduler-api.jar'
 ssh ubuntu@ec2-54-91-172-100.compute-1.amazonaws.com 'rm -f /usr/local/scheduler/scheduler-service.jar'
 ssh ubuntu@ec2-54-91-172-100.compute-1.amazonaws.com ln -s /usr/local/scheduler/scheduler-api-${VERSION}.jar /usr/local/scheduler/scheduler-api.jar
---
+++ schedulerapi/src/main/java/com/tracktik/scheduler/api/PingController.java
@@ -0,0 +1,26 @@
+package com.tracktik.scheduler.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+
+  private static final Logger logger = LoggerFactory.getLogger(PingController.class);
+
+  @Autowired
+  private JmsTemplate jmsTemplate;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public void getSchedule() {
+
+    return;
+  }
+
+}
