c6a907cca851e9231d3d7feeac9b80e95cb90141
dlianou <dlianou@gmail.com>
2020-04-01 12:48:38 -0400

    modified test_rest_post.sh file to include my local files

--- test_rest_post.sh
+++ test_rest_post.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-curl -i -X POST -H "Content-Type: application/json" --data-binary "@C:/Users/Dimitris Lianoudakis/ML_framework/tt-product/TrackTikOptaPlanner/data/export.json" localhost:8080/schedule
+#curl -i -X POST -H "Content-Type: application/json" --data-binary "@C:/Users/Dimitris Lianoudakis/ML_framework/tt-product/TrackTikOptaPlanner/data/export.json" localhost:8080/schedule
+curl -i -X POST -H "Content-Type: application/json" --data-binary "@C:/Users/dlian/TT-product/tt-product/TT_SAM/data/export.json" localhost:8080/schedule
 #curl -i -X POST -H "Content-Type: application/json" --data-binary "@./data/parameterized.json" ec2-54-91-172-100.compute-1.amazonaws.com:8080/schedule
